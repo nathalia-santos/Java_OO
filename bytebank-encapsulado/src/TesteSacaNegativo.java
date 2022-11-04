45fcde427ba597d200bfa3148589aa4ee1421bca
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-11-04 06:53:45 -0300

    construtor

--- bytebank-encapsulado/src/TesteSacaNegativo.java
+++ bytebank-encapsulado/src/TesteSacaNegativo.java
@@ -1,7 +1,7 @@
 
 public class TesteSacaNegativo {
 	public static void main(String[] args) {
-		Conta conta = new Conta();
+		Conta conta = new Conta(1337, 12440);
 		conta.deposita(100);
 		System.out.println(conta.saca(101));
 
